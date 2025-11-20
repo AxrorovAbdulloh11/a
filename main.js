0569222bf780f1afc4cb4d93d9a7e59d34377cd2
AxrorovAbdulloh11 <muxlisaahrolova@gmail.com>
2025-11-20 19:28:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// Function Declaration
+// function abdulloh(){
+//     console.log("abdulatif");
+// }
+// abdulloh()
+
+// Function Expression
+// const sayBye = function() {
+//     console.log("Bye!");
+// };
+// sayBye();
+
+// Arrow Function
+// const add = () => {
+//     console.log("Nma nma gap");
+// }
+// add()
+
+// Параметры и 
+// function greetUser(urod) {
+//     console.log("Salom, (urod)");
+// }
+// greetUser(abdulloh)
+// greetUser(abdulatif)
+
+
+
+
+
+
+
+
+function akam() {
+    alert("Sen oq fon xohlaganding")
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+}
+
+function ukam() {
+    alert("Sen qora fon xohlaganding")
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+}
